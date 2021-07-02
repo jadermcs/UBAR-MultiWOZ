615b10ae5106a13566592ceffb4633feed7b557a
Jader Martins <jadermcs94@gmail.com>
2021-07-01 21:54:08 -0300

    working demo, call it with demo mode flag

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from config import global_config as cfg 
+from config import global_config as cfg
 # from config21 import global_config as cfg  # global, already initialized
 
 
@@ -224,7 +224,7 @@ class Modal(object):
                                     for k, v in logs.items():
                                         self.tb_writer.add_scalar(
                                             k, v, global_step)
-                                # save model... 
+                                # save model...
 
                     except RuntimeError as exception:
                         if "out of memory" in str(exception):
@@ -344,7 +344,7 @@ class Modal(object):
                                 for k, v in logs.items():
                                     self.tb_writer.add_scalar(
                                         k, v, global_step)
-                            # save model... 
+                            # save model...
 
                 except RuntimeError as exception:
                     if "out of memory" in str(exception):
@@ -454,7 +454,7 @@ class Modal(object):
                             logging.info(str(exception))
                             logging.info(self.tokenizer.decode(generated_ar))
                             decoded = {'resp': [], 'bspn': [], 'aspn': []}
-                    
+
                     turn['resp_gen'] = decoded['resp']
                     turn['bspn_gen'] = turn['bspn'] if cfg.use_true_curr_bspn else decoded['bspn']
                     turn['aspn_gen'] = turn['aspn'] if cfg.use_true_curr_aspn else decoded['aspn']
@@ -473,7 +473,7 @@ class Modal(object):
                     # pv_turn['db'] = db
                     # pv_turn['aspn'] = turn['aspn'] if cfg.use_true_prev_aspn else decoded['aspn']
                     # pv_turn = inputs['labels']
-                    
+
                 result_collection.update(
                     self.reader.inverse_transpose_turn(dialog))
 
@@ -492,6 +492,173 @@ class Modal(object):
         eval_results['success'] = success
         eval_results['match'] = match
 
+        return eval_results
+
+    def demo(self, data='dev'):
+        # predict one dialog/ one turn at a time
+        from pprint import pprint
+        self.model.eval()
+
+        all_batches = self.reader.get_batches('dev')
+        data_iterator = self.reader.get_data_iterator(all_batches)
+        eval_data = self.reader.get_eval_data('dev')
+
+        # for token in eval_data[0][0]['user']:
+        #     print(self.tokenizer.decode(token), end=' ')
+        # print('\n\n')
+        # for token in eval_data[0][0]['resp']:
+        #     print(self.tokenizer.decode(token), end=' ')
+        # print('\n\n')
+        #out = self.model.generate(torch.cuda.LongTensor(eval_data[0][0]['user']))
+        #print(out)
+        #exit()
+
+        
+        set_stats = self.reader.set_stats[data]
+        logging.info("***** Running Evaluation *****")
+        logging.info("  Num Turns = %d", set_stats['num_turns'])
+        # logging.info("  Num Dialogs = %d", set_stats['num_dials'])
+
+        # valid_losses = []
+        btm = time.time()
+        result_collection = {}
+        with torch.no_grad():
+            for dial_idx, dialog in enumerate(eval_data):
+
+                pv_turn = {}
+                for turn_idx, turn in enumerate(dialog):
+                    first_turn = (turn_idx == 0)
+                    inputs = self.reader.convert_turn_eval(
+                        turn, pv_turn, first_turn)
+                    inputs = self.add_torch_input_eval(inputs)
+
+                    print("Please type something. Sugestion:", self.tokenizer.decode(eval_data[0][0]['user'])\
+                        .lstrip('<sos_u> ').rstrip('<eos_u>').capitalize())
+                    user_input = self.tokenizer.encode('<sos_u>'+input('>').lower()+'<eos_u>', add_special_tokens=True)
+
+                    print("\nTokenized version:")
+                    print(self.tokenizer.decode(user_input))
+                    print('\n\n')
+
+                    inputs['context_tensor'] = torch.cuda.LongTensor(user_input).reshape(1,-1)
+                    inputs['context'] = user_input
+                    # fail to generate new tokens, if max_length not set
+                    context_length = len(inputs['context'])
+                    if cfg.use_true_curr_bspn: # generate act, response
+                        max_len=60
+                        if not cfg.use_true_curr_aspn:
+                            max_len = 80
+
+                        outputs = self.model.generate(input_ids=inputs['context_tensor'],
+                                                    max_length=context_length+max_len, temperature=0.7, # top_p=0.9, num_beams=4,
+                                                    pad_token_id=self.tokenizer.eos_token_id, eos_token_id=self.tokenizer.encode(['<eos_r>'])[0])
+                                                    #   no_repeat_ngram_size=4
+                        # turn['generated'] = self.tokenizer.decode(outputs[0])
+
+                        #print(inputs)
+
+                        # resp_gen, need to trim previous context
+                        generated = outputs[0].cpu().numpy().tolist()
+                        generated = generated[context_length:]
+
+
+                        
+                        for token in generated:
+                            print(self.tokenizer.decode(token), end=' ')
+                        print('\n\n')
+                        exit()
+
+                        try:
+                            decoded = self.decode_generated_act_resp(generated)
+                        except ValueError as exception:
+                            logging.info(str(exception))
+                            logging.info(self.tokenizer.decode(generated))
+                            decoded = {'resp': [], 'bspn': [], 'aspn': []}
+
+                    else: # predict bspn, access db, then generate act and resp
+                        outputs = self.model.generate(input_ids=inputs['context_tensor'],
+                                                    max_length=context_length+60, temperature=0.7, # top_p=0.9, num_beams=4,
+                                                    pad_token_id=self.tokenizer.eos_token_id, eos_token_id=self.tokenizer.encode(['<eos_b>'])[0])
+                        generated_bs = outputs[0].cpu().numpy().tolist()
+                        # generated_bs = generated_bs[context_length-1:]
+                        bspn_gen = self.decode_generated_bspn(generated_bs[context_length-1:])
+                        # check DB result
+                        if cfg.use_true_db_pointer:
+                            # db_result = self.reader.bspan_to_DBpointer(self.tokenizer.decode(turn['bspn']), turn['turn_domain'])
+                            db = turn['db']
+                        else:
+                            db_result = self.reader.bspan_to_DBpointer(self.tokenizer.decode(bspn_gen), turn['turn_domain'])
+                            db = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize('<sos_db> '+ db_result + ' <eos_db>')) + self.tokenizer.encode(['<sos_a>'])
+                        inputs['context_tensor_db'] = torch.tensor([inputs['context'][:-1] + bspn_gen + db]).to(self.device)
+                        context_length = len(inputs['context_tensor_db'][0])
+                        outputs_db = self.model.generate(input_ids=inputs['context_tensor_db'],
+                                                    max_length=context_length+80, temperature=0.7, # top_p=0.9, num_beams=4,
+                                                    pad_token_id=self.tokenizer.eos_token_id, eos_token_id=self.tokenizer.encode(['<eos_r>'])[0])
+                        generated_ar = outputs_db[0].cpu().numpy().tolist()
+                        generated_ar = generated_ar[context_length-1:]
+                        try:
+                            decoded = self.decode_generated_act_resp(generated_ar)
+                            decoded['bspn'] = bspn_gen
+                        except ValueError as exception:
+                            logging.info(str(exception))
+                            logging.info(self.tokenizer.decode(generated_ar))
+                            decoded = {'resp': [], 'bspn': [], 'aspn': []}
+
+                    turn['resp_gen'] = decoded['resp']
+                    turn['bspn_gen'] = turn['bspn'] if cfg.use_true_curr_bspn else decoded['bspn']
+                    turn['aspn_gen'] = turn['aspn'] if cfg.use_true_curr_aspn else decoded['aspn']
+                    turn['dspn_gen'] = turn['dspn']
+
+                    # check DB results
+                    # db_result = self.reader.bspan_to_DBpointer(self.tokenizer.decode(turn['bspn']), turn['turn_domain'])
+                    # if db_result[0] == 1: # no match
+                    #     print('gt:', self.tokenizer.decode(turn['aspn']), '     |gen:', self.tokenizer.decode(decoded['aspn']))
+                    #     print('gen_resp: ', self.tokenizer.decode(decoded['resp']))
+                    #     print('gt_resp: ', self.tokenizer.decode(turn['resp']), '\n')
+
+                    pv_turn['labels'] = inputs['labels'] # all true previous context
+                    pv_turn['resp'] = turn['resp'] if cfg.use_true_prev_resp else decoded['resp']
+                    pv_turn['bspn'] = turn['bspn'] if cfg.use_true_prev_bspn else decoded['bspn']
+                    pv_turn['db'] = turn['db'] if cfg.use_true_curr_bspn else db
+                    pv_turn['aspn'] = turn['aspn'] if cfg.use_true_prev_aspn else decoded['aspn']
+
+                result_collection.update(
+                    self.reader.inverse_transpose_turn(dialog))
+
+        logging.info("inference time: {:.2f} min".format((time.time()-btm)/60))
+        # score
+        btm = time.time()
+        results, _ = self.reader.wrap_result_lm(result_collection)
+        bleu, success, match = self.evaluator.validation_metric(results)
+        logging.info("Scoring time: {:.2f} min".format((time.time()-btm)/60))
+        score = 0.5 * (success + match) + bleu
+        valid_loss = 130 - score
+        logging.info('validation [CTR] match: %2.2f  success: %2.2f  bleu: %2.2f    score: %.2f' % (
+            match, success, bleu, score))
+        eval_results = {}
+        eval_results['bleu'] = bleu
+        eval_results['success'] = success
+        eval_results['match'] = match
+        eval_results['score'] = score
+        eval_results['result'] = 'validation [CTR] match: %2.2f  success: %2.2f  bleu: %2.2f    score: %.2f' % (match, success, bleu, score)
+
+
+        model_setting, epoch_setting = cfg.eval_load_path.split('/')[1], cfg.eval_load_path.split('/')[2]
+        eval_on = '-'.join(cfg.exp_domains)
+        if data == 'test':
+            eval_on += '_test'
+        if not os.path.exists(cfg.log_path):
+            os.mkdir(cfg.log_path)
+        log_file_name = os.path.join(cfg.log_path, model_setting+'-'+eval_on+'.json')
+        if os.path.exists(log_file_name):
+            eval_to_json = json.load(open(log_file_name, 'r'))
+            eval_to_json[epoch_setting] = eval_results
+            json.dump(eval_to_json, open(log_file_name, 'w'), indent=2)
+        else:
+            eval_to_json = {}
+            eval_to_json[epoch_setting] = eval_results
+            json.dump(eval_to_json, open(log_file_name, 'w'), indent=2)
+        logging.info('update eval results to {}'.format(log_file_name))
         return eval_results
 
     def validate(self, data='dev', do_test=False):
@@ -572,7 +739,7 @@ class Modal(object):
                             logging.info(str(exception))
                             logging.info(self.tokenizer.decode(generated_ar))
                             decoded = {'resp': [], 'bspn': [], 'aspn': []}
-                    
+
                     turn['resp_gen'] = decoded['resp']
                     turn['bspn_gen'] = turn['bspn'] if cfg.use_true_curr_bspn else decoded['bspn']
                     turn['aspn_gen'] = turn['aspn'] if cfg.use_true_curr_aspn else decoded['aspn']
@@ -611,7 +778,7 @@ class Modal(object):
         eval_results['score'] = score
         eval_results['result'] = 'validation [CTR] match: %2.2f  success: %2.2f  bleu: %2.2f    score: %.2f' % (match, success, bleu, score)
 
-        
+
         model_setting, epoch_setting = cfg.eval_load_path.split('/')[1], cfg.eval_load_path.split('/')[2]
         eval_on = '-'.join(cfg.exp_domains)
         if data == 'test':
@@ -647,7 +814,7 @@ class Modal(object):
             eos_r_idx = len(generated)-1
             logging.info('eos_r not in generated: ' + self.tokenizer.decode(generated))
         # eos_r_idx = generated.index(eos_r_id) if eos_r_id in generated else len(generated)-1
-        
+
         if cfg.use_true_curr_aspn:  # only predict resp
             decoded['resp'] = generated[: eos_r_idx+1]
         else:  # predicted aspn, resp
@@ -655,7 +822,7 @@ class Modal(object):
             decoded['aspn'] = generated[: eos_a_idx+1]
             decoded['resp'] = generated[eos_a_idx+1: eos_r_idx+1]
         # if cfg.use_true_curr_bspn:
-            
+
         # else:  # predict bspn aspn resp
         #     eos_b_idx = generated.index(eos_b_id)
         #     eos_a_idx = generated.index(eos_a_id)
@@ -705,7 +872,7 @@ def main():
     args = parser.parse_args()
 
     cfg.mode = args.mode
-    if args.mode == 'test' or args.mode == 'adjust':
+    if args.mode == 'test' or args.mode == 'adjust' or args.mode == 'demo':
         parse_arg_cfg(args)
         # cfg.model_path = cfg.eval_load_path
         cfg.gpt_path = cfg.eval_load_path
@@ -716,7 +883,7 @@ def main():
             # cfg.exp_path = 'experiments/{}_{}_sd{}_lr{}_bs{}_sp{}_dc{}/'.format('-'.join(cfg.exp_domains),
             #                                                                     cfg.exp_no, cfg.seed, cfg.lr, cfg.batch_size,
             #                                                                     cfg.early_stop_count, cfg.weight_decay_count)
-            
+
             experiments_path = './experiments' if 'all' in cfg.exp_domains else './experiments_Xdomain'
             cfg.exp_path = os.path.join(experiments_path,'{}_{}_sd{}_lr{}_bs{}_ga{}'.format('-'.join(cfg.exp_domains),
                                                                           cfg.exp_no, cfg.seed, cfg.lr, cfg.batch_size,
@@ -737,7 +904,7 @@ def main():
         if len(cfg.cuda_device) == 1:
             cfg.multi_gpu = False
             # torch.cuda.set_device(cfg.cuda_device[0])
-            device = torch.device("cuda:{}".format(cfg.cuda_device[0]))
+            device = torch.device("cuda:0".format(cfg.cuda_device[0]))
         else:
             pass  # multi-gpu
     else:
@@ -765,6 +932,15 @@ def main():
             exit()
     elif args.mode == 'adjuest':
         pass
+    elif args.mode == 'demo':
+        logging.info("Generate setting: \n\t use true_prev_bspn={} \n\t use true_prev_aspn={} \n\t use true_db_pointer={} \n\t use true_prev_resp={} \n\t use true_curr_bspn={} \n\t use true_curr_aspn={} \n\t use_all_previous_context={}".format(
+                            cfg.use_true_prev_bspn, cfg.use_true_prev_aspn, cfg.use_true_db_pointer, cfg.use_true_prev_resp,
+                            cfg.use_true_curr_bspn, cfg.use_true_curr_aspn, cfg.use_all_previous_context
+                        ))
+        if cfg.context_scheme == 'UBARU':
+            m.demo()
+        else:
+            raise NotImplemented("Not running for URURU.")
     else:  # test
         logging.info("Generate setting: \n\t use true_prev_bspn={} \n\t use true_prev_aspn={} \n\t use true_db_pointer={} \n\t use true_prev_resp={} \n\t use true_curr_bspn={} \n\t use true_curr_aspn={} \n\t use_all_previous_context={}".format(
                             cfg.use_true_prev_bspn, cfg.use_true_prev_aspn, cfg.use_true_db_pointer, cfg.use_true_prev_resp,
